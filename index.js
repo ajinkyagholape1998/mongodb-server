d10f7cf655358b0848b02a53ebc817e595169b7d
Ajinkya Gholape <ajinkyagholape1998@gmail.com>
2022-05-15 11:32:54 +0530

    script added

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.post('/get-employees', async (request, response) => {
     try {
         await client.connect();
         const dbs = client.db("demodb");
-        const coll = dbs.collection("userinfo");
+        const coll = dbs.collection("employees");
 
         const cur = coll.find({}, {});
 
@@ -39,7 +39,7 @@ app.post('/create-employee', async (request, response) => {
         try {
             await client.connect();
             const dbs = client.db("demodb");
-            const coll = dbs.collection("userinfo");
+            const coll = dbs.collection("employees");
             const rest = await coll.insertOne(request.body);
 
             response.status(201).json(JSON.parse(JSON.stringify(request.body)));
@@ -76,3 +76,15 @@ var server = app.listen(8081, function () {
 
 module.exports = app;
 
+
+
+/*
+
+RUN PROJECT: npm start
+
+BackEnd URL: http://localhost:8081
+
+
+
+
+*/
